ed1ea56097d07794784ac4fa562b63135e78842d
jmbott <millerbott@gmail.com>
2017-03-04 09:39:14 -0500

    setup

--- Lab5/E6765_2017_JSJC_Lab5_jmb2341_sf2785_jc4609/A5_1.py
+++ Lab5/E6765_2017_JSJC_Lab5_jmb2341_sf2785_jc4609/A5_1.py
@@ -11,32 +11,6 @@
 # Additionally, store this data to the S3 storage.
 #
 
-import datetime
+from utils import mtaUpdates
 
-# Unix time is # of seconds since January 1, 1970 00:00 UTC
-hour = int(datetime.datetime.fromtimestamp(int(ts)).strftime('%H'))
-minute = int(datetime.datetime.fromtimestamp(int(ts)).strftime('%M'))
-# Timestamp in minutes past midnight
-m = hour*60 + minute
-
-
-# tripId/train start time
-# tripId: Constructed from the scheduled start time of the trip and a shape_id:
-# <start time>_<shape_id>. The start time is represented as hundredths of
-# minutes past midnight, six digits 0 padded. So, 6:45:30am would be
-# 040550.
-# 405.5/60 = 6 Remainder 0.7583
-# 0.7583*60 = 45 Remainder 0.49
-# minutes past midnight > parsed start time bit bit shifted into a float
-
-# routeId, local or express train
-
-# day of the week, weekday or Weekend
-
-# Time at which it reaches express station (at 96th street)
-# taken from the "vehicle message" of the MTA feed when possible
-# alt from "arrival time" from the 'trip_update' message
-
-# Time at which it reaches the destination (at 42nd Street)
-# taken from the "vehicle message" of the MTA feed when possible
-# alt from "arrival time" from the 'trip_update' message
+mtaUpdates.mtaUpdates().getTripUpdates()

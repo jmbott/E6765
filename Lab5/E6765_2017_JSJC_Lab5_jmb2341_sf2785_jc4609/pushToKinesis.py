2f9f6f2dad0129a1cc0d5c98daaaa39251916f81
6467121626 <jc4609@columbia.edu>
2017-03-08 21:45:14 -0500

    Update pushToKinesis.py

--- Lab5/E6765_2017_JSJC_Lab5_jmb2341_sf2785_jc4609/pushToKinesis.py
+++ Lab5/E6765_2017_JSJC_Lab5_jmb2341_sf2785_jc4609/pushToKinesis.py
@@ -10,12 +10,13 @@ import boto3
 sys.path.append('../utils')
 from utils import aws
 
-KINESIS_STREAM_NAME = 'mtaStream'
+
+KINESIS_STREAM_NAME = 'mtastream'
+kinesis = aws.getClient('kinesis','us-east-1')
 
 def main(fileName):
 
     # connect to kinesis
-    kinesis = aws.getClient('kinesis','us-east-1')
     data = [] # list of dictionaries will be sent to kinesis
     with open(fileName,'rb') as f:
     	dataReader = csv.DictReader(f)
@@ -28,14 +29,19 @@ def main(fileName):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Missing arguments"
-        sys.exit(-1)
-    if len(sys.argv) > 2:
-        print "Extra arguments"
-        sys.exit(-1)
+    
     try:
-        fileName = sys.argv[1]
+        fileName = "finalData-slim.csv"
         main(fileName)
+        Iterator = kinesis.get_shard_iterator(
+                    StreamName='mtastream',
+                    ShardId='shardId-000000000000',
+                    ShardIteratorType='TRIM_HORIZON'
+                    )
+        response = kinesis.get_records(
+                      ShardIterator=Iterator['ShardIterator'],
+                      Limit=2
+                      )
+        print response
     except Exception as e:
         raise e

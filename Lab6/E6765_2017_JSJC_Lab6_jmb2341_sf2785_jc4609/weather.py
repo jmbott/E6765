c621cc492b1fcc33604a0c7702ff76d87f49dea2
jmbott <millerbott@gmail.com>
2017-03-22 14:35:44 -0400

    post to database

--- Lab6/E6765_2017_JSJC_Lab6_jmb2341_sf2785_jc4609/weather.py
+++ Lab6/E6765_2017_JSJC_Lab6_jmb2341_sf2785_jc4609/weather.py
@@ -4,6 +4,14 @@
 
 import pywapi
 import time
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Lab6.settings'
+import django
+django.setup()
+
+from a2.models import C, F
+from django.utils import timezone
 
 # read temp from location in weather api
 def temp(station_id):
@@ -26,6 +34,17 @@ def temp(station_id):
     except:
         print("Random Gen Error")
 
+def post(temp, city_id):
+    try:
+        c = C(temp_c=int(float(temp['C'][:-1])), city=city_id, pub_date=timezone.now())
+        c.save()
+        f = F(temp_f=int(float(temp['F'][:-1])), city=city_id, pub_date=timezone.now())
+        f.save()
+    except KeyboardInterrupt:
+        exit
+    except:
+        print("Post Error")
+
 
 print("Press Ctrl+C to escape...")
 try:
@@ -42,7 +61,10 @@ try:
         bs_temp = temp('KEKS')
         sf_temp = temp('KSFO')
         # post to database
-
+        post(ny_temp, 1);
+        post(no_temp, 2);
+        post(bs_temp, 3);
+        post(sf_temp, 4);
         time.sleep(30)
 except KeyboardInterrupt:
         exit
